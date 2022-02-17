04d7c0694d88172eb0904d21aac656b9b785e80e
Hugo Souza <contato@hugosouza.com>
2022-02-17 13:28:55 -0400

    reformat code

--- Longinus.py
+++ Longinus.py
@@ -16,9 +16,18 @@ init()
 
 # TODO handle uninstalled modules
 
-def split_list(arr, parts):
-    k, m = divmod(len(arr), parts)
-    return (arr[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(parts))
+def are_from_same_domain(url1, url2):
+    return url1 in url2
+
+
+def compDom(URL1, URL2):
+    URL1Split = URL1.split(".")
+    URL2Split = URL2.split(".")
+    a = URL1Split[::-1]
+    b = URL2Split[::-1]
+    domain1 = a[1] + "." + a[0]
+    domain2 = b[1] + "." + b[0]
+    return domain1 == domain2
 
 
 def obtain_browser():
@@ -46,8 +55,20 @@ def obtain_browser():
     return browser
 
 
+# Strategies
+ONLY_ORIGIN_DOMAIN = 0  # doesnt follow links that lead to a different domain
+ONLY_SUBDOMAINS = 2  # follow only subdomains
+FOLLOW_ALL_LINKS = 3  # follows everything tat comes up on the page
+SHALLOW_LINKS = 4  # follows links that lead to other domains with depth 0
+
+
 class Longinus:
-    def __init__(self, name: str, threads: int = 4, wait_for_page_load_ms:int = 500):
+    class QueuedURL:
+        def __init__(self, url, depth):
+            self.url = url
+            self.depth = depth
+
+    def __init__(self, name: str, threads: int = 4, wait_for_page_load_ms: int = 500):
         self.name = name
         self.number_of_threads = threads
         self.threads = []
@@ -55,7 +76,11 @@ class Longinus:
         self.total_urls_crawled = 0
         self.total_urls = 0
         self.urls = ["https://hugosouza.com"]
+        self.queue = []
         self.wait = wait_for_page_load_ms
+        self.depth = 0
+        self.strategy = SHALLOW_LINKS
+        self.bonus = 1
 
         self.startup_message()
 
@@ -77,51 +102,83 @@ class Longinus:
         print(colored("BOT_NAME:", None, "on_green") + " " +
               colored(self.name, "green"))
         print(colored("NUMBER_OF_THREADS:", None, "on_green") + " " +
-              colored(self.number_of_threads, "green"))
+              colored(str(self.number_of_threads), "green"))
         print(colored("=" * 20, "green"))
 
-    def crawl(self, thread_id, depth: int, keywords: list, urls: list, browser, is_parent:bool=True):
-        self.total_urls += len(urls)
-        for url in urls:
+    def match_current_strategy(self, origin, link):
+        if self.strategy == FOLLOW_ALL_LINKS or self.strategy == SHALLOW_LINKS:
+            return True
+        elif self.strategy == ONLY_SUBDOMAINS:
+            return are_from_same_domain(origin, link)
+        elif self.strategy == ONLY_ORIGIN_DOMAIN:
+            return compDom(origin, link)
+
+    def get_new_depth(self, origin, link, current_depth):
+        if self.strategy == SHALLOW_LINKS and not are_from_same_domain(link, origin):
+            return 0
+        return current_depth-1
+
+    def hit_page(self, browser, url, thread_id=0):
+        start = time()
+        try:
+            browser.get(url)
+            sleep(self.wait / 1000)
+        except selenium.common.exceptions.WebDriverException:
+            self.log(colored("ERROR: Couldn't access url {}".format(url), "red"), thread=thread_id)
+        else:
+            self.log(colored("{} responded 200 in {:.2f} seconds".format(url, time() - start)), "green",
+                     thread=thread_id)
+
+        return browser.page_source
+
+    def queue_new_links(self, links, depth, url, thread_id=0):
+        if depth > 0:
+            page_links = 0
+            for link in links:
+                try:
+                    link = urljoin(url, link["href"])
+                except KeyError:
+                    continue
+                if (not link in self.crawled_links) and self.match_current_strategy(url, link):
+                    page_links += 1
+                    self.queue.append(self.QueuedURL(link, self.get_new_depth(url, link, depth)))
+                    self.total_urls += 1
+
+            if page_links != 0:
+                self.log(colored("{} new links found at {}".format(page_links, url), "blue"), thread=thread_id)
+
+    def crawl(self, thread_id, keywords: list, browser):
+        while len(self.queue) > 0:
+            current = self.queue.pop(0)
+            url = current.url
+            depth = current.depth
+
+            self.total_urls_crawled += 1
+
+            if url in self.crawled_links:
+                self.log(colored("Skipping {}...".format(url), "red"), thread=thread_id)
+                continue
+
             self.log(colored("({}/{})".format(self.total_urls_crawled, self.total_urls), "grey", "on_white") +
                      " Crawling over {} - Depth: {}".format(url, depth), thread=thread_id)
 
             self.crawled_links.append(url)
-            self.total_urls_crawled += 1
 
-            start = time()
-            try:
-                browser.get(url)
-                sleep(self.wait/1000)
-            except selenium.common.exceptions.WebDriverException:
-                self.log(colored("ERROR: Couldn't access url {}".format(url), "red"), thread=thread_id)
-            else:
-                self.log(colored("{} responded 200 in {:.2f} seconds".format(url, time() - start)), "green", thread=thread_id)
+            page_source = self.hit_page(browser, url, thread_id)
 
-            html = BeautifulSoup(browser.page_source, features="lxml")
+            html = BeautifulSoup(page_source, features="lxml")
 
             links = html.find_all('a')
 
             # SCRAPE DATA HERE
 
-            if depth > 0:
-                page_links = []
-                for link in links:
-                    link = urljoin(url, link["href"])
-                    if not (link in urls) and not (link in self.crawled_links):
-                        page_links.append(link)
-                
-                if len(page_links) != 0:
-                    self.log(colored("{} new links found at {}".format(len(page_links), url), "blue"), thread=thread_id)
+            self.queue_new_links(links, depth, url, thread_id)
 
-                self.crawl(thread_id, depth - 1, keywords, page_links, browser, False)
+        self.log(colored("Thread {} finished".format(thread_id), "red", "on_white"))
 
-        if is_parent:
-            self.log(colored("Thread {} finished".format(thread_id), "red", "on_white"))
-
-    def get_n_inside_links(self, url, browser, n):
+    def get_n_inside_links(self, url, browser, n, depth):
         browser.get(url)
-        sleep(self.wait/1000)
+        sleep(self.wait / 1000)
 
         try:
             browser.get(url)
@@ -133,12 +190,10 @@ class Longinus:
 
         links = html.find_all('a')
 
-        # SCRAPE DATA HERE
-
         page_links = []
         for link in links:
             link = urljoin(url, link["href"])
-            page_links.append(link)
+            page_links.append(self.QueuedURL(link, depth))
 
         if len(page_links) < n:
             new_links = self.get_n_inside_links(page_links[0], browser, n - len(page_links))
@@ -146,21 +201,32 @@ class Longinus:
 
         return page_links
 
-    def start(self, depth: int = 0, search_for=None):
-        self.log(colored("Starting crawling at depth {}".format(depth), "blue", "on_white"))
-        if search_for is None:
-            search_for = []
+    def setup(self, depth: int = 3, strategy=SHALLOW_LINKS, bonus_when_match=1):
+        for url in self.urls:
+            self.queue.append(self.QueuedURL(url, depth))
 
         if len(self.urls) < self.number_of_threads:
             temp_browser = obtain_browser()
-            self.urls += self.get_n_inside_links(self.urls[0], temp_browser, self.number_of_threads)
+            self.queue += self.get_n_inside_links(self.urls[0], temp_browser, self.number_of_threads, depth)
 
-        divided_urls = list(split_list(self.urls, self.number_of_threads))
+        self.depth = depth
+        self.strategy = strategy
+        self.bonus = bonus_when_match
+
+    def start(self, search_for=None):
+        if search_for is None:
+            search_for = []
+
+        self.log(colored("Starting crawling at depth {}".format(self.depth), "blue"))
+
+        self.total_urls = len(self.queue)
+
+        start = time()
 
         for thread_no in range(self.number_of_threads):
             self.log("Thread {} starting".format(thread_no))
 
-            t = threading.Thread(target=self.crawl, args=(thread_no + 1, depth, search_for, divided_urls[thread_no],
+            t = threading.Thread(target=self.crawl, args=(thread_no + 1, search_for,
                                                           obtain_browser()))
             t.start()
             self.threads.append(t)
@@ -168,8 +234,9 @@ class Longinus:
         for thread in self.threads:
             thread.join()
 
-        self.log(colored("Finished crawling", "blue", "on_white"))
+        self.log(colored("Finished crawling in {:.2f} seconds".format(time() - start), "blue"))
 
 
 longinus = Longinus("saint-longinus", 4)
-longinus.start(3)
+longinus.setup(depth=3)
+longinus.start()
